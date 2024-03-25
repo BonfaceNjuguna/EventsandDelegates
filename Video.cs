b838a3dd6f597b99bcfcff17d41767b47e218510
Bonface Njuguna <bonfacenjuguna13@gmail.com>
2024-03-25 20:15:09 +0300

    Add project files.

---
+++ Video.cs
@@ -0,0 +1,7 @@
+﻿namespace EventsandDelegates
+{
+    public class Video
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ VideoEncoder.cs
@@ -0,0 +1,27 @@
+﻿namespace EventsandDelegates
+{
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Video { get; set; }
+    }
+    public class VideoEncoder
+    {
+        //EventHandler
+        //2 - Define an event based on that delegate
+        public event EventHandler<VideoEventArgs> VideoEncoded;
+
+        //3 - Raise the event
+        protected virtual void OnVideoEncoded(Video video)
+        {
+            if (VideoEncoded != null)
+                VideoEncoded(this, new VideoEventArgs() { Video = video });
+        }
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding video...");
+            Thread.Sleep(3000);
+
+            OnVideoEncoded(video);
+        }
+    }
+}
